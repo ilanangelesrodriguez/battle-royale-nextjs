668f82f538f92cabc099c1b349e51d1f1ba98dd4
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2024-01-13 11:11:16 -0500

    feat: agregar modulo de dialogo

--- components/Dialog/Dialog.tsx
+++ components/Dialog/Dialog.tsx
@@ -1,1 +1,24 @@
+import React from "react";
+import styles from "./Dialog.module.css";
+import { Button } from "../Button";
+
+interface Props {
+    children: React.ReactNode;
+}
+
+function Dialog({ children }: Props) {
+    const closeDialog = () => {
+        console.log("close dialog");
+    };
+    return (
+    <div className={styles["dialog-background"]}>
+        <dialog className={styles["dialog"]}>
+            {children}
+            <Button callBack={closeDialog}>Close</Button>
+        </dialog>
+    </div>
+    );
+}
+
+export default Dialog;
 
